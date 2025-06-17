8afc0057b0bc93d2f38e811729ae77cd8ffc8ba6
Keerthana Motte <kemo22aiml@cmrit.ac.in>
2025-06-17 18:13:26 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-import librosa
-import numpy as np
-
-def extract_features(file):
-    audio, sr = librosa.load(file, res_type='kaiser_fast')
-    mfcc = librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40)
-    return np.mean(mfcc.T, axis=0)
